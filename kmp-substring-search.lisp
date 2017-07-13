80d81ae9598a1f30d2978ebefa279871b1a92966
Manoel Vilela <manoel_vilela@engineer.com>
2017-07-13 12:58:16 -0300

    KMP: refactoring code, now 100% loop macro
    YES!!! LOOP MACRO YES!

--- kmp-substring-search.lisp
+++ kmp-substring-search.lisp
@@ -2,51 +2,50 @@
 ;; Date: Thu 13 Jul 2017 08:48:40 AM -03
 ;; Manoel Vilela
 
+;; NOTE: I'm programming in Common Lisp or in LOOP-MACRO?
 
 (defun make-prefix-table (pattern length)
   "Get the shift prefix array table to use on KMP
    algorithm. This value is used to shift string comparison
    on the next evaluation after a mismatch"
-  (let ((prefix-table (make-array length :initial-element 0))
-        (prefix 0))
-    (loop for i from 1 below length
-          do (loop while (and (> prefix 0)
-                              (not (eq (aref pattern i)
-                                       (aref pattern prefix))))
-                   do (setq prefix (aref prefix-table (1- prefix))))
-          when (eq (aref pattern i)
-                   (aref pattern prefix))
-            do (incf prefix)
-          end
-          do (setf (aref prefix-table i) prefix)
-          finally (return prefix-table))))
+  (loop with prefix-table = (make-array length :initial-element 0)
+         and prefix = 0
+        for i from 1 below length
+        do (loop while (and (> prefix 0)
+                            (not (eq (aref pattern i)
+                                     (aref pattern prefix))))
+                 do (setq prefix (aref prefix-table (1- prefix))))
+        when (eq (aref pattern i)
+                 (aref pattern prefix))
+          do (incf prefix)
+        end
+        do (setf (aref prefix-table i) prefix)
+        finally (return prefix-table)))
 
 
 (defun find-substring (pattern string)
   "Using the KMP algorithm, find the position of the pattern in string if exists
    Otherwise return nil."
-  (let* ((pattern-length (length pattern))
-         (string-length (length string))
-         (prefix (make-prefix-table pattern pattern-length))
-         (string-index 0)
-         (pattern-index 0))
-    (when (> pattern-length string-length)
-      (return-from find-substring nil))
-    (loop while (< string-index string-length)
-          when (eq (aref string string-index)
+  (loop with string-length = (length string)
+         and pattern-length = (length pattern)
+         and string-index = 0
+         and pattern-index = 0
+        with prefix = (make-prefix-table pattern pattern-length)
+        while (< string-index string-length)
+        when (eq (aref string string-index)
                  (aref pattern pattern-index))
-            do (progn (incf string-index)
-                      (incf pattern-index))
-          if (= pattern-index pattern-length)
-            return (- string-index pattern-index)
-          else
-            if (and (< string-index string-length)
-                    (not (eq (aref string string-index)
-                             (aref pattern pattern-index))))
-              if (/= pattern-index 0)
-                do (setq pattern-index (aref prefix (1- pattern-index)))
-              else
-                do (incf string-index))))
+          do (progn (incf string-index)
+                    (incf pattern-index))
+        if (= pattern-index pattern-length)
+          return (- string-index pattern-index)
+        else
+          if (and (< string-index string-length)
+                  (not (eq (aref string string-index)
+                           (aref pattern pattern-index))))
+            if (/= pattern-index 0)
+              do (setq pattern-index (aref prefix (1- pattern-index)))
+        else
+          do (incf string-index)))
 
 (defun main ()
   (loop repeat (read)
